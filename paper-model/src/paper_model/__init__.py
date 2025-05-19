27823edd7ac324d05b11c80a143d76a3127b9238
Lorenzo Varese <55581163+lorenzovarese@users.noreply.github.com>
2025-05-19 15:53:04 +0200

    Define entry point for run_pipeline in paper-model

--- paper-model/src/paper_model/__init__.py
+++ paper-model/src/paper_model/__init__.py
@@ -4,5 +4,6 @@ __author__ = "Lorenzo Varese"
 __version__ = "0.1.0"
 
 from .manager import ModelManager
+from .run_pipeline import main
 
-__all__ = ["ModelManager"]
+__all__ = ["ModelManager", "main"]

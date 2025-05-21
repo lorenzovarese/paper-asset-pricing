4585629f18ba9471eb882ea4c302e05683f0b4ff
Lorenzo Varese <55581163+lorenzovarese@users.noreply.github.com>
2025-05-21 16:26:39 +0200

    Add HuberLinearModel implementation and update model imports

--- src/paperassetpricing/models/__init__.py
+++ src/paperassetpricing/models/__init__.py
@@ -2,7 +2,7 @@ from .base_model import BaseModel
 from .model_registry import get_model, list_models, register_model
 
 # import built-in models so they register on import
-from .linear_model import LinearModel
+from .linear_model import LinearModel, HuberLinearModel
 from .neural_network_model import NeuralNetworkModel
 
 __all__ = [
@@ -11,5 +11,6 @@ __all__ = [
     "list_models",
     "register_model",
     "LinearModel",
+    "HuberLinearModel",
     "NeuralNetworkModel",
 ]
